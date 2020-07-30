8299817110df4ac169b152c4e8bb683d4f2db5e2
NEb0 <neb0@empherino.net>
2020-07-30 11:12:44 +0300

    add TimestampableTrait

---
+++ src/Entity/Traits/TimestampableTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait TimestampableTrait
+{
+    /**
+     * @var \DateTimeImmutable $createdAt
+     *
+     * @ORM\Column(name="created_at", type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTimeImmutable $updatedAt
+     *
+     * @ORM\Column(name="updated_at", type="datetime_immutable")
+     */
+    private $updatedAt;
+
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function onPrePersist()
+    {
+        $this->updatedAt = $this->createdAt = new \DateTimeImmutable('now');
+    }
+
+    public function onPreUpdate()
+    {
+        $this->updatedAt = new \DateTimeImmutable('now');
+    }
+}
