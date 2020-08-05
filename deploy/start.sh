f3513bdbac6593d3df7ab21dd1445ccc4c75e7a0
NEb0 <neb0@empherino.net>
2020-08-05 11:11:14 +0300

    fix launching container

--- deploy/start.sh
+++ deploy/start.sh
@@ -6,5 +6,8 @@ sleep 5
 cd /root/oauth-test
 bin/console doctrine:database:drop --if-exists -f -n
 bin/console doctrine:database:create
+echo 'load DB dump'
 mysql oauth_test <deploy/data.sql
+echo 'starting server'
 php -S 0.0.0.0:8000 -t public/ &
+/bin/bash
